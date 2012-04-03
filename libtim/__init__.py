5d403146f4159e752adf4f4c7587db3ae7e4efdc
Tim van Werkhoven <timvanwerkhoven@gmail.com>
2012-04-03 11:43:17 +0200

    Update description, date, etc.
    This file was copied from a different project, adapted to this new
    project

--- libtim/__init__.py
+++ libtim/__init__.py
@@ -1,31 +1,28 @@
-#!/usr/bin/env python2.5
+#!/usr/bin/env python
 # encoding: utf-8
 """
-This is the libwfs module, providing routines for wavefront sensor data 
-processing.
+This is Tim's library for useful stuff.
 
-The docstrings provide concise runtime information. For more elaborate 
+The docstrings provide concise runtime information. For more elaborate
 documentation, consult the doxygen documentation.
 
-Created by Tim van Werkhoven (t.i.m.vanwerkhoven@xs4all.nl) on 2010-05-19
-Copyright (c) 2010 Tim van Werkhoven. All rights reserved.
+Created by Tim van Werkhoven (werkhoven@strw.leidenuniv.nl) on 2012-04-03
+Copyright (c) 2010--2012 Tim van Werkhoven. All rights reserved.
 
 This file is licensed under the Creative Commons Attribution-Share Alike
 license versions 3.0 or higher, see
 http://creativecommons.org/licenses/by-sa/3.0/
 """
 
-__all__ = ["libwfs"]
+__all__ = ["libtim"]
 __version__ = "v0.1.0"
 
-## @mainpage libwfs
-# 
-# This is the libwfs module, providing routines for wavefront sensor data 
-# processing.
-
-## @package libwfs
-# @brief Main package for wavefront sensor 
-# @author Tim van Werkhoven (t.i.m.vanwerkhoven@xs4all.nl)
+## @mainpage libtim
 #
-# This is the libwfs module, providing routines for wavefront sensor data 
-# processing.
+# This is libtim, providing various useful things
+
+## @package libtim
+# @brief Main package in libtim
+# @author Tim van Werkhoven (werkhoven@strw.leidenuniv.nl)
+#
+# This is libtim, providing various useful things
