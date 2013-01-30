7ea27752f3f47f6e703c224c810249161402b78d
Tim van Werkhoven <timvanwerkhoven@gmail.com>
2013-01-30 10:18:40 +0100

    Minor code rewrite, add flexibility
    calc_cog: fixed bug for asymmetric input data, return data ordered by
    axes
    find_mla_grid: option to copy data or not (Default is to copy), add
    option to tweak return data format

--- libtim/shwfs.py
+++ libtim/shwfs.py
@@ -13,52 +13,67 @@
 This module provides functions to analyze Shack-Hartmann wavefront sensor data
 """
 
-#=============================================================================
+#==========================================================================
 # Import libraries here
-#=============================================================================
+#==========================================================================
 
 import sys
 import os
-import numpy as N
+import numpy as np
 import unittest
 
-#=============================================================================
+#==========================================================================
 # Defines
-#=============================================================================
+#==========================================================================
 
-#=============================================================================
+#==========================================================================
 # Routines
-#=============================================================================
+#==========================================================================
 
 def calc_cog(img):
 	"""
-	Calculate center of gravity
+	Calculate center of gravity for a given 2D image for dimensions 0 and 1. 
+
+	N.B. Because of data ordering, dimension 0 (1) is **not** the x-axis 
+	(y-axis) when plotting!
 	
-	@param [in] img
-	@return Sub-pixel coordinate of center of gravity (c0, c1)
+	@param [in] img input image
+	@return Sub-pixel coordinate of center of gravity ordered by data dimension (c0, c1)
 	"""
+
+	ims = img.sum()
 	
-	c0 = (img.sum(0) * N.arange(img.shape[0])).sum()/img.sum()
-	c1 = (img.sum(1) * N.arange(img.shape[1])).sum()/img.sum()
+	# For dimension 0, sum all but dimension 0 (which in 2D is only dim 1)
+	c0 = (img.sum(1) * np.arange(img.shape[0])).sum()/ims
+	c1 = (img.sum(0) * np.arange(img.shape[1])).sum()/ims
+
 	return (c0, c1)
 
-def find_mla_grid(wfsimg, size, minif=0.6, nmax=-1):
+def find_mla_grid(wfsimg, size, minif=0.6, nmax=-1, copy=True, method='bounds'):
 	"""
-	Given a wavefront sensor image, find a grid of subapertures (sa) that 
-	match the image.
+	Given a Shack-hartmann wave front sensor image, find a grid of 
+	subapertures (sa) of approximately **size** big.
+
+	The image will be destroyed during analysis, unless **copy** is True 
+	(default)
 	
 	@param [in] wfsimg Image to analyze
 	@param [in] size Subaperture size [pixels]
-	@param [in] minif Minimimum intensity a pixel has to have to consider it as a new subaperture, as fraction of the maximum intensity.
-	@param [in] nmax Maximum number of subapertures to search for
+	@param [in] minif Minimimum pixel value to have to consider it as a new subaperture, as fraction of the global maximum.
+	@param [in] nmax Maximum number of subapertures to search for (-1 for no max)
+	@param [in] copy Copy image before modifying
+	@param [in] method Coordinate format to return, either 'bounds' or 'center'
+	@return list of subaperture coordinates
+
+	Raises ValueError if subapertures size is larger than source image.
 	"""
 	
 	if (size[0] > wfsimg.shape[0] or size[1] > wfsimg.shape[1]):
-		raise ValueError("Subapertures sizes larger than the soure image resolution don't make sense.")
+		raise ValueError("Subapertures sizes larger than the source image resolution don't make sense.")
 	
 	# Copy image because we destroy it
-	img = wfsimg.copy()
-	
+	if (copy): wfsimg = wfsimg.copy()
+
 	# Minimum intensity to consider
 	min_int = wfsimg.max() * minif
 	
@@ -66,36 +81,39 @@ def find_mla_grid(wfsimg, size, minif=0.6, nmax=-1):
 	
 	while (True):
 		# The current maximum intesnity is at:
-		currmax = img.max()
-		maxidx = N.argwhere(img == currmax)[0]
+		currmax = wfsimg.max()
+		maxidx = np.argwhere(wfsimg == currmax)[0]
 		
 		if (currmax < min_int):
 			break
 		
-		# Add this subaperture
-		newsa = (maxidx[0] - size[0]/2, 
-				maxidx[0] + size[0]/2,
-				maxidx[1] - size[1]/2,
-				maxidx[1] + size[1]/2)
+		# Add this subaperture, either by bounds or central coordinate
+		if (method == 'bounds'):
+			newsa = (maxidx[0] - size[0]/2, 
+					maxidx[0] + size[0]/2,
+					maxidx[1] - size[1]/2,
+					maxidx[1] + size[1]/2)
+		else:
+			newsa = tuple(maxidx)
 		subap_grid.append(newsa)
 		
-		# Clear out subaperture so we don't add it again
-		img[newsa[0]:newsa[1], newsa[2]:newsa[3]] = img.min()
+		# Clear out this subaperture so we don't add it again
+		wfsimg[newsa[0]:newsa[1], newsa[2]:newsa[3]] = wfsimg.min()
 		
 		if (nmax > 0 and len(subap_grid) >= nmax):
 			break
 	
 	return subap_grid
 
-def calc_subap_grid(rad, size, pitch, shape='circular', xoff=[0,0.5], disp=(0,0), overlap=1.0, scl=1.0):
+def calc_subap_grid(rad, size, pitch, shape='circular', xoff=(0, 0.5), disp=(0,0), overlap=1.0, scl=1.0):
 	"""
-	Generate subaperture (sa) positions for a given configuration.
+	Generate regular subaperture (sa) positions for a given configuration.
 
 	@param rad radius of the sa pattern (before scaling) [pixels]
 	@param size size of the sa's [pixels]
 	@param pitch pitch of the sa's [pixels]
 	@param shape shape of the sa pattern ['circular' or 'square']
-	@param xoff the horizontal position offset of odd rows (in units of 'size')
+	@param xoff the horizontal position offset of odd rows (in units of 'size'), set to 0.5 for hexagonal grids
 	@param disp global displacement of the sa positions [pixels]
 	@param overlap how much overlap should the subap have to be included (0=complete, 1=any overlap)
 	@param scl global scaling of the sa positions [pixels]
@@ -106,14 +124,15 @@ def calc_subap_grid(rad, size, pitch, shape='circular', xoff=[0,0.5], disp=(0,0)
 	we found using the specified configuration.
 	"""
 
-	rad = N.array(rad)
-	size = N.array(size)
-	disp = N.array(disp)
-	pitch = N.array(pitch)
-	xoff = N.array(xoff)
+	# Convert to arrays just to be sure
+	rad = np.array(rad)
+	size = np.array(size)
+	disp = np.array(disp)
+	pitch = np.array(pitch)
+	xoff = np.array(xoff)
 
 	# (half) width and height of the subaperture array
-	sa_arr = (N.ceil(rad/pitch)+1).astype(int)
+	sa_arr = (np.ceil(rad/pitch)+1).astype(int)
 	# Init empty list to store positions
 	pos = []
 	# Loop over all possible subapertures and see if they fit inside the
@@ -146,7 +165,7 @@ def calc_subap_grid(rad, size, pitch, shape='circular', xoff=[0,0.5], disp=(0,0)
 
 	# Apply scaling and displacement to the pattern before returning
 	# NB: pos gives the *centroid* position of the subapertures here
-	cpos = (N.array(pos) * scl) + disp
+	cpos = (np.array(pos) * scl) + disp
 
 	# Convert symmetric centroid positions to origin positions:
 	llpos = cpos - size/2.0
@@ -155,12 +174,5 @@ def calc_subap_grid(rad, size, pitch, shape='circular', xoff=[0,0.5], disp=(0,0)
 
 	return (nsa, llpos, cpos, size)
 
-### TEST SUITES
-
-# arr = N.zeros((16,16))
-# arr[10,10] = 1000
-# arr[11,11] = 1000
-# print tim.shwfs.calc_cog(arr)
-	
 if __name__ == "__main__":
 	sys.exit(unittest.main())
