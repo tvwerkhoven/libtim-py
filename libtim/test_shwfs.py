5fbf7fc97ac07444f0b7918897ce7aa1e2b59f9d
Tim van Werkhoven <timvanwerkhoven@gmail.com>
2013-01-30 18:02:35 +0100

    Add test routine

--- libtim/test_shwfs.py
+++ libtim/test_shwfs.py
@@ -23,18 +23,25 @@ class TestCoG(unittest.TestCase):
 		self.maxpos = [(15, 5), (17, 17), (63, 0), (50, 50), (50, 50)]
 		for im, rim, p in zip(self.imlist, self.rndimlist, self.maxpos):
 			im[p] = 10
-			rim[p] = 10
+			# The peak should be ten times higher than the background noise to get <0.1 pixel CoG noise (on average this will give <0.05 pixel noise)
+			rim[p] = np.product(np.r_[rim.shape])*10
 	
 	def test0_try_calc_cog(self):
 		"""Try to see if calc_cog"""
 		for im in self.imlist:
 			cog = calc_cog(im)
 	
-	def test1_check_calc_cog(self):
+	def test1a_check_calc_cog(self):
 		"""Try to see if calc_cog output is  sane"""
 		for im, p in zip(self.imlist, self.maxpos):
 			cog = calc_cog(im)
 			self.assertEqual(cog, p)
+
+	def test1b_check_calc_cog(self):
+		"""Try to see if calc_cog output is  sane"""
+		for rim, p in zip(self.rndimlist, self.maxpos):
+			cog = calc_cog(rim)
+			self.assertLess((p[0]-cog[0]) + (p[1]-cog[1]), 0.2)
 
 if __name__ == "__main__":
 	import sys
