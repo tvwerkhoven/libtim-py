0f17a30b38f9f33ce279de9a07549dfcf0d072e1
Tim van Werkhoven <timvanwerkhoven@gmail.com>
2013-07-26 16:06:27 +0200

    Using column-vector convention now.
    Now using column-vector notation such that a matrix A of (x,y) and
    matrix B of (y,z) give C = A . B = np.dot(A, B) with C (x, z).

--- libtim/ao.py
+++ libtim/ao.py
@@ -14,18 +14,18 @@ import numpy as np
 import pylab as plt
 from os.path import join as pjoin
 
-def comp_influence(inflmeas, inflact, binfac=None, singval=1.0, add_offset=False, use_zernike=False, verb=0):
+def comp_influence(measmat, actmat, binfac=None, singval=1.0, add_offset=False, use_zernike=False, verb=0):
 	"""
 	Calculate the system influence matrix and its inverse from calibration 
 	data, using **singval** as cut-off for the singular value decomposition.
 
 	Given:
 
-		(1)   error + measmat = [infl_mat, offset] . [actmat, 1]
+		(1)   measmat = [infl_mat, offset] . [actmat, 1]
 
 	with **measmat** and **actmat** known, we solve for **infl_mat**:
 
-		(2)   (error + measmat) . [actmat, 1]^+ = [infl_mat, offset]
+		(2)   measmat . [actmat, 1]^+ = [infl_mat, offset]
 
 	where **actmat^+** is the pseudo inverse of **actmat** using all 
 	singular values.
@@ -33,72 +33,75 @@ def comp_influence(inflmeas, inflact, binfac=None, singval=1.0, add_offset=False
 	We check this solution with:
 
 		(3a)  epsilon = measmat - infl_mat . actmat
-		(3b)  epsilon = measmat^+ . (infl_mat . actmat) - identity
-		(3c)  measmat_rec = infl_mat . actmat
-		(3d)  actmat_rec = measmat . infl_mat^+
+		(3b)  measmat_rec = infl_mat . actmat
+		(3c)  actmat_rec = measmat . infl_mat^+
 
 	where **epsilon** should be small compared to the mean of **measmat**.
 
 	Finally we obtain the control matrix from the inverse of the influence 
 	matrix, **infl_mat^+** by singular value decomposing **infl_mat** 
-	using a limited set of singular values for regularisation. We check this 
-	solution with:
+	using a limited set of singular values for regularisation:
 
-		(4)   measmat_rec = infl_mat . actmat
-		(5)   measmat . infl_mat^+ = actmat
+		(4a)  ctrl_mat = infl_mat^+
+		(4b)  U, s, V^H = svd(infl_mat)
+		(4c)  ctrl_mat = V . diag(1/s) . U^T
 
-	@param [in] inflmeas Influence measurement data matrix
-	@param [in] inflact Influence actuation data matrix
-	@param [in] binfac Bin measurement data (inflmeas, inflact) by this factor for speed
+	where ctrl_mat is (n_act, n_meas)
+
+	@param [in] measmat Calibration measurements, size (n_data, n_meas)
+	@param [in] actmat Calibration actuations (n_act, n_meas)
+	@param [in] binfac Bin measurement data (measmat) by this factor
 	@param [in] verb Verbosity
-	@return Dictionary with keys inflmat, epsilon, offsetvec, ctrlmat, svdcomps (as U, s, Vh))
+	@return Dictionary with keys inflmat, epsilon, offsetmeas, offsetact, ctrlmat, svdcomps (as U, s, Vh))
 	"""
 
 	infl = {}
 
+	# Check matrix size, measmat should be (n_data, n_meas), actmat should 
+	# be (n_act, n_meas). We should have:
+	# n_data > n_meas
+	# n_meas > n_act
+	n_data, n_meas1 = measmat.shape
+	n_act, n_meas2 = actmat.shape
+	assert (n_data > n_meas1), "Measurement matrix wrong, transposed?"
+	assert (n_meas2 > n_act), "Actuation matrix wrong, transposed?"
+	assert (n_meas2 == n_meas1), "Matrices incompatible, not the same number of measurements"
+
 	if (binfac):
 		# Check if this binfac divides the size to an integer
-		if (inflmeas.shape[1]/(binfac*1.0) == int(inflmeas.shape[1]/(binfac*1.0))):
-			inflmeas = sum([inflmeas[::, i::binfac] for i in range(binfac)])/binfac
+		if (measmat.shape[0]/(binfac*1.0) == int(measmat.shape[0]/(binfac*1.0))):
+			measmat = sum([measmat[i::binfac] for i in range(binfac)])/binfac
 		else:
-			warnings.warn("Not binning, factor not divisor!")
+			warnings.warn("Not binning, factor not a divisor!")
 	
-	if (use_zernike):
-		pass
-		# thisinflmeas = []
-		# thismode = np.zeros(CAM_CFG['mask'].shape)
-		# for m in self.inflmeas:
-		# 	thismode[CAM_CFG['mask']] = m
-		# 	thiszern = tim.zern.fit_zernike(thismode, zern_data=self.zern_cache, nmodes=self.nmodes, rec_zern=False)[0]
-		# 	thisinflmeas.append(thiszern)
-		# thisinflmeas = np.r_[thisinflmeas]
-
 	# Add offset vector if requested
 	if (add_offset):
-		inflact = np.hstack([ inflact, np.ones_like(inflact[:,0:1]) ])
+		actmat = np.vstack([ actmat, np.ones_like(actmat[0:1]) ])
 
 	# 2. Compute the influence matrix
-	inflmat = np.dot(np.linalg.pinv(inflact), inflmeas)
+	inflmat = np.dot(measmat, np.linalg.pinv(actmat))
 	
 	# 3a. Check the quality as a fraction of mismatch for influence measurements
-	infl['epsilon'] = np.abs(inflmeas - np.dot(inflact, inflmat)).mean() / np.abs(inflmeas).mean()
+	infl['infleps'] = np.abs(measmat - np.dot(inflmat, actmat)).mean() / np.abs(measmat).mean()
 
 	# Remove offset mode, use for static correction
-	infl['offsetvec'] = inflmat[-1:]
-	# @bug @todo This was required for data read from disk?
-	infl['inflmat'] = (inflmat[:-1]).newbyteorder('=')
+	infl['offsetmeas'] = inflmat[:, -1:]
+	infl['inflmat'] = inflmat[:, :-1]
 
+	# 4b. Decompose influence matrix with SVD
 	infl['svdcomps'] = svd_U, svd_s, svd_Vh = np.linalg.svd(infl['inflmat'], full_matrices=False)
 	nmodes = lambda s, perc: np.argwhere(s.cumsum()/s.sum() >= perc)[0][0] if s.sum() else 0
 	svd_s_red = svd_s.copy()
 	svd_s_red[nmodes(svd_s, singval)+1:] = np.inf
 
-	infl['ctrlmat'] = np.dot(svd_Vh.T.conj(), np.dot(np.diag(1.0/svd_s_red), svd_U.T.conj()))
+	# 4c. Compute (regularized) control matrix
+	# V . diag(1/s) . U^T
+	infl['ctrlmat'] = np.dot(svd_Vh.conj(), np.dot(np.diag(1.0/svd_s_red), svd_U.T))
 
 	# 3c. Check the quality in reconstructing the actuation matrix
-	infl['actmat_rec'] = np.dot(inflmeas - infl['offsetvec'], infl['ctrlmat'])
-
-	infl['offcorrvec'] = offvec = -np.dot(infl['offsetvec'], infl['ctrlmat'])
+	infl['actmat_rec'] = np.dot(infl['ctrlmat'],measmat - infl['offsetmeas'])
+	infl['acteps'] = np.abs(infl['actmat_rec'] - actmat[:-1]).mean()
+	infl['offsetact'] = offvec = -np.dot(infl['ctrlmat'], infl['offsetmeas'])
 
 	if (np.abs(offvec).max() > 1.0):
 		print "Offset correction actuation, reg.: ", " ".join("%.2g" % e for e in offvec.flat)
@@ -109,18 +112,30 @@ def comp_influence(inflmeas, inflact, binfac=None, singval=1.0, add_offset=False
 
 	return infl
 
-def inspect_influence(inflact, inflmeas, apt_mask, infldat=None, what="all", fignum0=1000, store=False, outdir='./'):
+def inspect_influence(actmat, measmat, apt_mask, infldat=None, what="all", fignum0=1000, store=False, outdir='./'):
 	"""
 	Given influence data, inspect it
 	"""
 	
-	if infldat == None: 
-		infldat = comp_influence(inflmeas, inflact)
-	svd_U, svd_s, svd_Vh = infldat['svdcomps']
-	offsetvec = infldat['offsetvec'].ravel()
+	# Check matrix size, measmat should be (n_data, n_meas), actmat should 
+	# be (n_act, n_meas). We should have:
+	# n_data > n_meas
+	# n_meas > n_act
+	n_data, n_meas1 = measmat.shape
+	n_act, n_meas2 = actmat.shape
+	assert (n_data > n_meas1), "Measurement matrix wrong, transposed?"
+	assert (n_meas2 > n_act), "Actuation matrix wrong, transposed?"
+	assert (n_meas2 == n_meas1), "Matrices incompatible, not the same number of measurements"
 
+	if infldat == None: 
+		infldat = comp_influence(measmat, actmat)
+	
+	svd_U, svd_s, svd_Vh = infldat['svdcomps']
+	offsetmeas = infldat['offsetmeas'].ravel()
 	dshape = apt_mask.shape
 	apt_mask_c = im.mk_rad_mask(*dshape) < 0.9
+	plot_mask = np.ones(apt_mask.shape)
+	plot_mask[apt_mask==False] = np.nan
 
 	if (what in ["all", "singval"]):
 		plt.figure(fignum0+100); plt.clf()
@@ -142,14 +157,13 @@ def inspect_influence(inflact, inflmeas, apt_mask, infldat=None, what="all", fig
 		# Init new empty image
 		thisvec = np.zeros(dshape)
 		# Fill image with masked vector data
-		thisvec[apt_mask] = offsetvec.ravel()
-		thisvec[apt_mask == False] = offsetvec.mean()
-		imin, imax = thisvec[apt_mask_c].min(), thisvec[apt_mask_c].max()
+		thisvec[apt_mask] = offsetmeas
+		imin, imax = offsetmeas.min(), offsetmeas.max()
 		plt.figure(fignum0+200); plt.clf()
 		plt.xlabel("X [pix]")
 		plt.ylabel("Y [pix]")
 		plt.title("Offset phase (system aberration)")
-		plt.imshow(thisvec, vmin=imin, vmax=imax)
+		plt.imshow(thisvec*plot_mask, vmin=imin, vmax=imax)
 		plt.colorbar()
 		if (store): plt.savefig(pjoin(outdir, "cal_offset_phase.pdf"))
 		raw_input("Continue...")
@@ -157,35 +171,34 @@ def inspect_influence(inflact, inflmeas, apt_mask, infldat=None, what="all", fig
 	if (what in ["all", "actrec"]):
 		plt.figure(fignum0+250); plt.clf()
 		plt.title("Actuation matrix reconstruction")
-		vmin, vmax = inflact.min(), inflact.max()
+		vmin, vmax = actmat.min(), actmat.max()
 		plt.subplot(131)
 		plt.xlabel("Actuator [id]")
 		plt.ylabel("Measurement [#]")
-		plt.imshow(inflact, vmin=vmin, vmax=vmax)
+		plt.imshow(actmat.T, vmin=vmin, vmax=vmax)
 		plt.subplot(132)
 		plt.xlabel("Actuator [id]")
-		plt.imshow(infldat['actmat_rec'], vmin=vmin, vmax=vmax)
+		plt.imshow(infldat['actmat_rec'].T, vmin=vmin, vmax=vmax)
 		plt.colorbar()
 		plt.subplot(133)
 		plt.xlabel("Actuator [id]")
-		plt.imshow(inflact[:,:-1]-infldat['actmat_rec'])
+		plt.imshow((actmat[:-1]-infldat['actmat_rec']).T)
 		plt.colorbar()
 		if (store): plt.savefig(pjoin(outdir, "cal_actmat_rec.pdf"))
 		raw_input("Continue...")
 
-	if (what in ["all", "inflact"]):
+	if (what in ["all", "actmat"]):
 		# Init new empty image
 		thisvec = np.zeros(dshape)
-		for idx, inflvec in enumerate(infldat['inflmat']):
+		for idx, inflvec in enumerate(infldat['inflmat'].T):
 			thisvec[apt_mask] = inflvec.ravel()
-			thisvec[apt_mask == False] = inflvec.mean()
-			imin, imax = thisvec[apt_mask_c].min(), thisvec[apt_mask_c].max()
+			imin, imax = inflvec.min(), inflvec.max()
 
 			plt.figure(fignum0+300); plt.clf()
 			plt.xlabel("X [pix]")
 			plt.ylabel("Y [pix]")
 			plt.title("Actuator %d influence" % (idx))
-			plt.imshow(thisvec, vmin=imin, vmax=imax)
+			plt.imshow(thisvec*plot_mask, vmin=imin, vmax=imax)
 			plt.colorbar()
 			if (store): plt.savefig(pjoin(outdir, "cal_infl_vec_%d.pdf" % (idx)))
 			if (raw_input("Continue [b=break]...") == 'b'): break
@@ -193,33 +206,31 @@ def inspect_influence(inflact, inflmeas, apt_mask, infldat=None, what="all", fig
 	if (what in ["all", "sysmodes"]):
 		# Init new empty image
 		thisbase = np.zeros(dshape)
-		for idx, base in enumerate(svd_Vh):
+		for idx, base in enumerate(svd_U.T):
 			thisbase[apt_mask] = base.ravel()
-			thisbase[apt_mask == False] = base.mean()
-			imin, imax = thisbase[apt_mask_c].min(), thisbase[apt_mask_c].max()
+			imin, imax = base.min(), base.max()
 
 			plt.figure(fignum0+400); plt.clf()
 			plt.xlabel("X [pix]")
 			plt.ylabel("Y [pix]")
 			plt.title("DM base %d, s=%g (%g)" % (idx, svd_s[idx], svd_s[idx]/svd_s.sum()))
-			plt.imshow(thisbase, vmin=imin, vmax=imax)
+			plt.imshow(thisbase*plot_mask, vmin=imin, vmax=imax)
 			plt.colorbar()
 			if (store): plt.savefig(pjoin(outdir, "cal_dm_base_vec_%d.pdf" % (idx)))
 			if (raw_input("Continue [b=break]...") == 'b'): break
 
 	if (what in ["all", "measrec"]):
-		measmat_rec = np.dot(inflact[:,:-1], infldat['inflmat'])
+		measmat_rec = np.dot(infldat['inflmat'], actmat[:-1])
 		thismeasrec = np.zeros(dshape)
-		for idx, measvec_rec in enumerate(measmat_rec):
+		for idx, measvec_rec in enumerate(measmat_rec.T):
 			thismeasrec[apt_mask] = measvec_rec.ravel()
-			thismeasrec[apt_mask == False] = measvec_rec.mean()
-			imin, imax = thismeasrec[apt_mask_c].min(), thismeasrec[apt_mask_c].max()
+			imin, imax = measvec_rec.min(), measvec_rec.max()
 
 			plt.figure(fignum0+500); plt.clf()
 			plt.xlabel("X [pix]")
 			plt.ylabel("Y [pix]")
 			plt.title("influence meas. %d rec" % (idx))
-			plt.imshow(thismeasrec, vmin=imin, vmax=imax)
+			plt.imshow(thismeasrec*plot_mask, vmin=imin, vmax=imax)
 			plt.colorbar()
 			if (store): plt.savefig(pjoin(outdir, "cal_infl_vec_%d_rec.pdf" % (idx)))
 			if (raw_input("Continue [b=break]...") == 'b'): break
